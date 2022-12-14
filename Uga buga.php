f0fa9c59fa69660ca67549e59da6c95d9ee22a94
Técnico em Informática 2022.2 <36127512022.2@senacrjedu.local>
2022-12-14 16:27:37 -0300

    inicio php

---
+++ Uga buga.php
@@ -0,0 +1,16 @@
+<?php
+$num1 = 6351;
+$num2 = 6848;
+
+$soma = $num1 + $num2;
+
+  if($soma >= 1200){
+    echo "maior que 1200";}
+else {
+    echo "menor que 1200"; }
+
+
+echo "<br/> o resultado da soma é {$soma}";
+
+?>
+
